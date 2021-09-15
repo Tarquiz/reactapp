1e80432e24c0d13e0241670bec9b4ce5f5979536
Tarquiz <79832105+Tarquiz@users.noreply.github.com>
2021-09-15 12:42:11 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import Todos from './components/Todos.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <Todos/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ Todos.js
@@ -0,0 +1,38 @@
+import React,{useState} from 'react';
+
+function Todos () {
+
+    const [desc, setDesc] = useState({description: '', date: ''});
+    const [todos, setTodos] = useState([]);
+
+    const addTodos = (event) => {
+        event.preventDefault();
+        setTodos ([...todos, desc])
+    }
+
+    const inputChanged = (event) => {
+        setDesc({...desc, [event.target.name]: event.target.value})
+    }
+    
+    return (
+        <div>
+           Date: <input type='Date' name='date' value={desc.date} onChange={inputChanged}/>
+           Todo: <input type='text' name='description' value={desc.description} onChange={inputChanged}/>
+            <button onClick={addTodos}>Add</button>
+            <table>
+                <tbody>
+                    <tr><th>Tehtävä</th><th>Date</th> </tr>
+                    {
+                        todos.map((todo, index) => 
+                        <tr key= {index}>
+                            <td>{todo.description}</td>
+                            <td>{todo.date}</td>
+                            </tr>
+                            )
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+export default Todos;
